41f966b0463ca523476a58473a4be2641e9c1d87
Asial Jim <asialjim@hotmail.com>
2025-04-25 08:59:34 +0800

    init

--- commons.sh
+++ commons.sh
@@ -15,11 +15,11 @@
 # limitations under the License.
 #
 
-#cd
-#git clone git@github.com:MicroApplet/commons.git
-#cd commons
-#$MAVEN_HOME/bin/mvn clean install -DskipTests=true
-#cd
-#rm -rf commons
+cd
+git clone git@github.com:MicroApplet/commons.git
+cd commons
+$MAVEN_HOME/bin/mvn clean install -DskipTests=true
+cd
+rm -rf commons
 
-docker run -v /app asialjim/maven-with-gpg:jdk8 install --repository clone git@github.com:MicroApplet/commons.git
+#docker run -v /app asialjim/maven-with-gpg:jdk8 install --repository clone git@github.com:MicroApplet/commons.git
